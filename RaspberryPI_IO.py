2210df17acb8ca58beca398d2b1ddcbb75adc4ed
MSH <mjsh635@gmail.com>
2020-02-10 12:42:46 -0500

    comment out the class

--- RaspberryPI_IO.py
+++ RaspberryPI_IO.py
@@ -20,52 +20,97 @@ class RPIO:
         pin 27 => +   1
         pin 22 => + 0.1
         """
-        # self.GPIO = GPIO
-        self.GPIO.setwarnings(False)
+        # variables to be used throughout the class
+        self.last_set_value = 0.0
+        self.relay_settle_time = 0.0675
+
+        # self.GPIO.setwarnings(False)
+        # Set the mode to BCM (the chip used in RPI)
+        # this determines how the pins should be 
+        # addressed.
         self.GPIO.setmode(self.GPIO.BCM)
-        self.pins = [4,17,27,22]
-        for pin in self.pins:
+
+        # Pin array that should be set to outputs
+        self.output_pins = [4,17,27,22]
+        # Loop over the pin array setting each pin
+        # to an output as it goes
             self.GPIO.setup(pin, self.GPIO.OUT)
 
     def _home_settings(self):
+        """Set the power setting to zero by
+        subtracting 10% 10 times
+        """
+        # Loop for 10 cycles to zero the power level out
         for _ in range(1,11):
+            # Toggle the power ON
             self.GPIO.output(4, True)
             print(f"minus {10*_}",end='\r')
-            self.time.sleep(0.0675)
+            # Time for the relay to settle
+            self.time.sleep(self.relay_settle_time)
+            # Toggle the power OFF
             self.GPIO.output(4, False)
-        print("Value homed at 0 %")
+        print("Value homed at 0 %                           ")
             
 
     def _add_10_percent(self, tens : int):
+        """Add 10% power level 
+        """
+        # Loop for the required 10's to add
         for _ in range(1,tens+1):
+            # Toggle relay ON
             self.GPIO.output(17, True)
             print(f"adding {10*_}",end='\r')
-            self.time.sleep(0.0675)
+            # time for the relay to settle
+            self.time.sleep(self.relay_settle_time)
+            # Toggle relay OFF
             self.GPIO.output(17, False)
-        print(f"added {10*tens}               ")
+        print(f"added {10*tens}                              ")
 
     def _add_1_percent(self, ones:int):
+        """Add 1% power level
+        """
+        # Loop for the required 1's to add
         for _ in range(1,ones+1):
+            # Toggle relay ON
             self.GPIO.output(27, True)
             print(f"adding {1*_}",end='\r')
-            self.time.sleep(0.0675)
+            # Time for the relay to settle
+            self.time.sleep(self.relay_settle_time)
+            # Toggle relay OFF
             self.GPIO.output(27, False)
-        print(f"added {1*ones}               ")
+        print(f"added {1*ones}                              ")
 
 
-    def _add_point_1_percent(self,pnt_ones:int):
+    def _add_point_1_percent(self, pnt_ones:int):
+        """Add 0.1% power level
+        """
+        # Loop for the required 0.1's to add
         for _ in range(1,pnt_ones+1):
+            # Toggle relay ON
             self.GPIO.output(22, True)
             print(f"adding {float}",end='\r')
-            self.time.sleep(0.0675)
+            # Time for the relay to settle
+            self.time.sleep(self.relay_settle_time)
+            # Toggle relay OFF
             self.GPIO.output(22, False)
         print(f"\nadded {float(0.1)*pnt_ones}               ")
 
     def get_value(self):
-        return NotImplemented
+        """Return the last set power level
+        """
+        return self.last_set_value
 
     def set_value(self, target: float) -> bool:
-        return NotImplemented
+        """Set the power level desired and return if
+        completed successfully
+        """
+        # count tens and then subtract from total
+        #count remaining ones then subtract from total
+        #remainder should be point ones
+        #call the functions for their values
+        #if completed okay then set last set value
+        #This should be change, return true if
+        #completed okay
 
 
 rpi = RPIO()
