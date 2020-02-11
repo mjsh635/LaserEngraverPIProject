2e3be13b7f1ae5acdca358c8ff4d77ae745cdb88
MSH <mjsh635@gmail.com>
2020-02-10 21:20:33 -0500

    temp webpage for setting power

---
+++ FlaskWebpage.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+#from RaspberryPI_IO import RPIO as my_pi
+from Test import test as my_pi
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET", "POST"])
+def start():
+
+    if request.method == "POST":
+        num = 0.0
+        num = float(request.form["Power_value"])
+        my_pi().set_value(num)
+
+    return """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Set Power Level</title>
+</head>
+<body>
+    <h1>SET POWER LEVEL</h1>
+    <form method="POST">
+        Desired Power Value: <br>
+        <input type="text" name="Power_value">
+        <input type="submit",text="Set Power Level">
+    </form>
+</body>
+</html>
+    """
+
+
+app.run(debug=True)
