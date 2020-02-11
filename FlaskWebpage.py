224ec585429c8958152664627b78792d228b79e1
MSH <mjsh635@gmail.com>
2020-02-11 09:58:09 -0500

    adjusting to increase readability

--- FlaskWebpage.py
+++ FlaskWebpage.py
@@ -11,22 +11,23 @@ def start():
         num = float(request.form["Power_value"])
         my_pi().set_value(num)
 
-    return """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Set Power Level</title>
-</head>
-<body>
-    <h1>SET POWER LEVEL</h1>
-        Desired Power Value: <br>
-        <input type="text" name="Power_value">
-        <input type="submit",text="Set Power Level">
-    </form>
-</body>
-</html>
-    """
+    return  """<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Set Power Level</title>
+                </head>
+                <body>
+                    <h1>SET POWER LEVEL</h1>
+                    <form method="POST">
+                        Desired Power Value: <br>
+                        <input type="text" name="Power_value">
+                        <input type="submit",text="Set Power Level">
+                    </form>
+                </body>
+                </html>
+            """
 
 
 app.run(debug=True,host='192.168.0.252')
